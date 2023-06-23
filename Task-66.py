04765c7d690d5bccf8db624ac9c0e34118477d8f
Плясунов Яков <132136416+Yakovplyasunov@users.noreply.github.com>
2023-06-23 21:21:53 +0300

    Create Task-66.py

---
+++ Task-66.py
@@ -0,0 +1,19 @@
+def count_substrings(s):
+    count = 0
+    char_freq = {}
+
+    for char in s:
+        if char in char_freq:
+            char_freq[char] += 1
+        else:
+            char_freq[char] = 1
+
+    for freq in char_freq.values():
+        count += (freq * (freq - 1)) // 2
+
+    return count
+
+# Пример использования
+input_string = "abcab"
+result = count_substrings(input_string)
+print(result)
